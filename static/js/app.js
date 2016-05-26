133b2a963e68af2091508a51e29d3c6565095c87
Jean-Baptiste <contact@jbpasquier.eu>
2016-05-26 15:04:33 +0200

    Add React Router

--- static/js/app.js
+++ static/js/app.js
@@ -20,8 +20,7 @@
 
         render() {
             var rows = [];
-            this.state.todos.forEach(function(todo,id){
-                console.log(todo);
+            this.state.todos.forEach(function(todo,id) {
                 rows.push(
                     <li key={id}>{todo.description}</li>
                 );
@@ -33,8 +32,54 @@
             );
         }
     }
-    ReactDOM.render(
-        <List source="api/todos/" />,
-        document.getElementById('app')
-    );
+    class Menu extends React.Component {
+        render() {
+            return (
+                <div>
+                    <ul><li><a href="/#/">GET</a></li><li><a href="/#/add">POST</a></li><li><a href="/#/delete">DELETE</a></li></ul>
+                </div>
+            )
+        }
+    }
+    class TodosList extends React.Component {
+        render() {
+            return (
+                <div>
+                    <List source="api/todos/" />
+                </div>
+            )
+        }
+    }
+    class TodosAdd extends React.Component {
+        render() {
+            return (
+                <div>
+                    <h1>Add a todo form here</h1>
+                </div>
+            )
+        }
+    }
+    class TodosDelete extends React.Component {
+        render() {
+            return (
+                <div>
+                    <h1>Here we delete a todo</h1>
+                </div>
+            )
+        }
+    }
+    var Route = ReactRouter.Route;
+    var Routes = ReactRouter.Routes;
+    var Link = ReactRouter.Link;
+    ReactDOM.render((
+        <div>
+            <Menu />
+            <ReactRouter.Router>
+                <ReactRouter.Route path="/" component={TodosList}/>
+                <ReactRouter.Route path="/add" component={TodosAdd}/>
+                <ReactRouter.Route path="/delete" component={TodosDelete}/>
+            </ReactRouter.Router>
+        </div>
+    ), document.getElementById('app'));
+
 })();
