ee9a585de1b3e8f85d698e4bf22abba4bf8b014e
Jean-Baptiste <contact@jbpasquier.eu>
2016-05-26 15:26:36 +0200

    Better view

--- static/js/app.js
+++ static/js/app.js
@@ -22,21 +22,45 @@
             var rows = [];
             this.state.todos.forEach(function(todo,id) {
                 rows.push(
-                    <li key={id}>{todo.description}</li>
+                    <tr key={id}>
+                        <td>
+                            <input type='text' className='form-control' value={todo.description} />
+                        </td>
+                        <td className='text-center'>
+                            <span className='glyphicon glyphicon-edit'></span>
+                        </td>
+                        <td className='text-center'>
+                            <span className='glyphicon glyphicon-remove'></span>
+                        </td>
+                    </tr>
                 );
             });
             return (
-                <ul>
-                    {rows}
-                </ul>
+                <table className='table table-hover table-bordered'>
+                    <thead>
+                        <tr><th>Todo</th><th>Modifier</th><th>Supprimer</th></tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </table>
             );
         }
     }
     class Menu extends React.Component {
         render() {
             return (
-                <div>
-                    <ul><li><a href="/#/">GET</a></li><li><a href="/#/add">POST</a></li><li><a href="/#/delete">DELETE</a></li></ul>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <h1>React &amp; Django - Todo List</h1>
+                    </div>
+                    <div className='row'>
+                        <h2>Test router</h2>
+                        <ul><li><a href="/#/">GET</a></li><li><a href="/#/add">POST</a></li><li><a href="/#/delete">DELETE</a></li></ul>
+                    </div>
+                    <div className='row'>
+                        <h2>Page</h2>
+                    </div>
                 </div>
             )
         }
