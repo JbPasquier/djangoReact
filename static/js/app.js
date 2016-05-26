2757498cecae018cd0937326a980d4aeba673b8d
Jean-Baptiste <contact@jbpasquier.eu>
2016-05-26 12:02:20 +0200

    Add todo get

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,37 @@
 (function(){
-ReactDOM.render(
-        <h1>Hello, world!</h1>,
-        document.getElementById('app')
-      );
+  class List extends React.Component {
+    constructor() {
+      super();
+      this.state = { todos: [] }
+    }
+
+          componentDidMount() {
+            this.serverRequest = $.get(this.props.source, function (result) {
+              this.setState({
+                todos: result
+              });
+            }.bind(this));
+          }
+
+          componentWillUnmount() {
+            this.serverRequest.abort();
+          }
+
+    render() {
+      var rows = [];
+      console.log(this.state);
+      this.state.todos.forEach(function(todo){
+          rows.push(<li key={todo.id}>{todo.description}</li>)
+      })
+      return (
+        <ul>
+          {rows}
+        </ul>
+      )
+    }
+  }
+    ReactDOM.render(
+      <List source="api/todos/" />,
+      document.getElementById('app')
+    );
 })();
